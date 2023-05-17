a59bf9aecd1b68b33ea18c8e17e2131f3c8c9d6c
dyschaf <schaffeldovid@gmail.com>
2023-05-17 19:06:03 +0300

    working

--- src/multiwaterfall/Calculator.js
+++ src/multiwaterfall/Calculator.js
@@ -7,49 +7,13 @@ import Accordion from 'react-bootstrap/Accordion';
 import Hurdle from './Hurdle';
 import Fee from './Fee';
 import Investor from './Investor';
-const Calculator = () => {
-    const [waterfall_info, setWaterfallInfo] = useState( {"name": "Gary's LLC",
-       'irr_parri_passu': true,
-       'pay_gp_principal': false,
-       'pay_gp_prefered': false,
-       'type_of_split': "split_plus_percentage",
-       'capital_parri_passu': false,
-       'type_of_hurdle': "irr_yearly_compund",
-       'way_in_which_to_split' : "investor_in_the_irr",
-       'principal_after_preffered': false,
-       'yr_strt_capital_payback': (10000 - 1),
-       "splits": [
-        {
-            'hurdle' : .08,
-                    'sponsor_percent' : 0,
-                    'limited_partner_percent' : 1 },
+const Calculator = ({waterfall, setWaterfall, personId ,test}) => {
     
-    
-      ],
-       "amount_gp_invested": 100000,
-       "amount_lp_invested": 0,
-       "fees": [
-        {
-            'year' : 1000,
-            'before_what_hurdle' : 1,
-            'who_gets_this_fee' : 'NEED AN INPUT',
-            'percentage_or_cash' : 324,
-            'type_transaction' : 'cash',
-            'type_of_fee' : 'hurdle'
-        }
-        
-      ],
-      "investors": [
-        {
-        }
-    
-      ],
-      }
-)
-
-    function handleWaterfallInfoChange(data, mortgageInfoType) {
+  console.log(personId)
+  console.log(test)
+    function handlewaterfallChange(data, mortgageInfoType) {
       let keyvalue = mortgageInfoType.toString()
-      setWaterfallInfo(prevState => ({
+      setWaterfall(prevState => ({
           ...prevState,
           [`${keyvalue}`]: data
       }));
@@ -57,7 +21,7 @@ const Calculator = () => {
 
       
       const SendPromoteWaterfall = (e) => {
-        setWaterfallInfo(prevState => ({
+        setWaterfall(prevState => ({
           ...prevState,
           [`irr_parri_passu`]: true,
           [`type_of_split`]: "split_percentage_of_owned",
@@ -69,7 +33,7 @@ const Calculator = () => {
 
     }
     const SendCashFlowWaterfall = (e) => {
-      setWaterfallInfo(prevState => ({
+      setWaterfall(prevState => ({
         ...prevState,
         [`irr_parri_passu`]: false,
         [`pay_gp_principal`]: true,
@@ -110,11 +74,11 @@ const Calculator = () => {
         
         <Form.Group as={Col} controlId="formGridEmail">
           <Form.Label>GP amount sponsered</Form.Label>
-          <Form.Control  placeholder={waterfall_info.amount_gp_invested} onChange={(e) => handleWaterfallInfoChange(e.target.value,'amount_gp_invested')} />
+          <Form.Control  placeholder={waterfall[personId].amount_gp_invested} onChange={(e) => handlewaterfallChange(e.target.value,'amount_gp_invested')} />
         </Form.Group>        
         <Form.Group as={Col} controlId="formGridEmail">
           <Form.Label>LP amount sponsored (optional if no input of investers)</Form.Label>
-          <Form.Control  placeholder={waterfall_info.amount_lp_invested} onChange={(e) => handleWaterfallInfoChange(e.target.value,'amount_lp_invested')} />
+          <Form.Control  placeholder={waterfall[personId].amount_lp_invested} onChange={(e) => handlewaterfallChange(e.target.value,'amount_lp_invested')} />
         </Form.Group>
         
         <Form.Group as={Col} controlId="formGridEmail">
@@ -122,12 +86,12 @@ const Calculator = () => {
         type="switch"
         id="custom-switch"
         label="pay back principal after preferred"
-        checked={waterfall_info.principal_after_preffered}
-        onChange={(e) => waterfall_info.principal_after_preffered ? handleWaterfallInfoChange(false,'principal_after_preffered') : handleWaterfallInfoChange(true,'principal_after_preffered')}
+        checked={waterfall[personId].principal_after_preffered}
+        onChange={(e) => waterfall[personId].principal_after_preffered ? handlewaterfallChange(false,'principal_after_preffered') : handlewaterfallChange(true,'principal_after_preffered')}
       />        </Form.Group>
         <Form.Group as={Col} controlId="formGridEmail">
         <Form.Label>Year start paying back principal</Form.Label>
-        <Form.Control  placeholder={waterfall_info.yr_strt_capital_payback} onChange={(e) => handleWaterfallInfoChange(e.target.value,'yr_strt_capital_payback')} />
+        <Form.Control  placeholder={waterfall[personId].yr_strt_capital_payback} onChange={(e) => handlewaterfallChange(e.target.value,'yr_strt_capital_payback')} />
     </Form.Group>
 
       </Row>
@@ -135,8 +99,8 @@ const Calculator = () => {
           
         <Form.Group as={Col} controlId="formGridEmail">
         <Form.Label>Type of split</Form.Label>
-        <Form.Select aria-label="Default select example" onChange={(e) => handleWaterfallInfoChange(e.target.value,'type_of_split')}>
-              <option value={waterfall_info.type_of_split}>{waterfall_info.type_of_split}</option>
+        <Form.Select aria-label="Default select example" onChange={(e) => handlewaterfallChange(e.target.value,'type_of_split')}>
+              <option value={waterfall[personId].type_of_split}>{waterfall[personId].type_of_split}</option>
               <option value="straight">straight split</option>
               <option value="split_plus_percentage">split_plus_percentage</option>
               <option value="split_percentage_of_owned">split_percentage_of_owned</option>
@@ -144,16 +108,16 @@ const Calculator = () => {
           </Form.Group>
           <Form.Group as={Col} controlId="formGridEmail">
         <Form.Label>Way In which to split</Form.Label>
-        <Form.Select aria-label="Default select example" onChange={(e) => handleWaterfallInfoChange(e.target.value,'way_in_which_to_split')}>
-              <option value={waterfall_info.way_in_which_to_split}>{waterfall_info.way_in_which_to_split}</option>
+        <Form.Select aria-label="Default select example" onChange={(e) => handlewaterfallChange(e.target.value,'way_in_which_to_split')}>
+              <option value={waterfall[personId].way_in_which_to_split}>{waterfall[personId].way_in_which_to_split}</option>
               <option value="investor_in_the_irr">Hurdle Bar IRR return for total invested</option>
               <option value="investor_on_top_of_irr">Hurdle Bar IRR return for LP</option>
           </Form.Select>
           </Form.Group>
           <Form.Group as={Col} controlId="formGridEmail">
         <Form.Label>Type of hurdle</Form.Label>
-        <Form.Select aria-label="Default select example"  onChange={(e) => handleWaterfallInfoChange(e.target.value,'type_of_hurdle')}>
-              <option value={waterfall_info.type_of_hurdle}>{waterfall_info.type_of_hurdle}</option>
+        <Form.Select aria-label="Default select example"  onChange={(e) => handlewaterfallChange(e.target.value,'type_of_hurdle')}>
+              <option value={waterfall[personId].type_of_hurdle}>{waterfall[personId].type_of_hurdle}</option>
               <option value="non_yearly_compund">non yearly compund </option>
               <option value="irr_yearly_compund">irr yearly compund</option>
               <option value="paydown_capital_paydown_irr">paydown capital paydown irr</option>
@@ -165,26 +129,26 @@ const Calculator = () => {
           type="switch"
           id="custom-switch"
           label="irr_parri_passu"
-          checked={waterfall_info.irr_parri_passu}
-          onChange={(e) => waterfall_info.irr_parri_passu ? handleWaterfallInfoChange(false,'irr_parri_passu') : handleWaterfallInfoChange(true,'irr_parri_passu')}
+          checked={waterfall[personId].irr_parri_passu}
+          onChange={(e) => waterfall[personId].irr_parri_passu ? handlewaterfallChange(false,'irr_parri_passu') : handlewaterfallChange(true,'irr_parri_passu')}
         />        </Form.Group>
-        { !waterfall_info.irr_parri_passu  && <>
+        { !waterfall[personId].irr_parri_passu  && <>
           <Form.Group as={Col} controlId="formGridEmail">
           <Form.Check 
           type="switch"
           id="custom-switch"
           label="pay_gp_prefered"
-          checked={waterfall_info.pay_gp_prefered}
-          onChange={(e) => waterfall_info.pay_gp_prefered ? handleWaterfallInfoChange(false,'pay_gp_prefered') : handleWaterfallInfoChange(true,'pay_gp_prefered')}
+          checked={waterfall[personId].pay_gp_prefered}
+          onChange={(e) => waterfall[personId].pay_gp_prefered ? handlewaterfallChange(false,'pay_gp_prefered') : handlewaterfallChange(true,'pay_gp_prefered')}
         />        </Form.Group>
           <Form.Group as={Col} controlId="formGridEmail">
           <Form.Check 
           type="switch"
           id="custom-switch"
           label="pay_gp_principal"
-          checked={waterfall_info.pay_gp_principal}
+          checked={waterfall[personId].pay_gp_principal}
   
-          onChange={(e) => waterfall_info.pay_gp_principal ? handleWaterfallInfoChange(false,'pay_gp_principal') : handleWaterfallInfoChange(true,'pay_gp_principal')}
+          onChange={(e) => waterfall[personId].pay_gp_principal ? handlewaterfallChange(false,'pay_gp_principal') : handlewaterfallChange(true,'pay_gp_principal')}
         />        </Form.Group>
         </>
         }
@@ -193,9 +157,9 @@ const Calculator = () => {
           type="switch"
           id="custom-switch"
           label="capital_parri_passu"
-          checked={waterfall_info.capital_parri_passu}
+          checked={waterfall[personId].capital_parri_passu}
   
-          onChange={(e) => waterfall_info.capital_parri_passu ? handleWaterfallInfoChange(false,'capital_parri_passu') : handleWaterfallInfoChange(true,'capital_parri_passu')}
+          onChange={(e) => waterfall[personId].capital_parri_passu ? handlewaterfallChange(false,'capital_parri_passu') : handlewaterfallChange(true,'capital_parri_passu')}
         />        </Form.Group>
       
         </Row>
@@ -205,19 +169,19 @@ const Calculator = () => {
       <Accordion.Item eventKey="2">
         <Accordion.Header>Split and Hurdle Information</Accordion.Header>
         <Accordion.Body>
-        <Hurdle waterfall_info={waterfall_info} setWaterfallInfo={setWaterfallInfo}></Hurdle>
+        <Hurdle waterfall={waterfall} setWaterfall={setWaterfall} personId={personId} ></Hurdle>
 
               </Accordion.Body>
       </Accordion.Item>  
        <Accordion.Item eventKey="3">
         <Accordion.Header>Fee's (optional)</Accordion.Header>
         <Accordion.Body>
-    <Fee waterfall_info={waterfall_info} setWaterfallInfo={setWaterfallInfo}></Fee>
+    <Fee waterfall={waterfall} setWaterfall={setWaterfall} personId={personId}></Fee>
               </Accordion.Body>
       </Accordion.Item>     <Accordion.Item eventKey="4">
         <Accordion.Header>Add Investors (optional)</Accordion.Header>
         <Accordion.Body>
-          <Investor waterfall_info={waterfall_info} setWaterfallInfo={setWaterfallInfo}></Investor>
+          <Investor waterfall={waterfall} setWaterfall={setWaterfall} personId={personId}></Investor>
 
               </Accordion.Body>
       </Accordion.Item>
@@ -230,7 +194,7 @@ const Calculator = () => {
 
 </Row>
 
-<button onClick={() => console.log(waterfall_info)}>check Info</button>
+<button onClick={() => console.log(waterfall)}>check Info</button>
       
       
       </div>   
